8c2ea9388a50d550f25eba7354ec4f332dc46c69
benedekaibas <kaibas.benedek02@gmail.com>
2025-06-17 21:25:29 +0200

    finished the window opening function

--- games/sprite.cpp
+++ games/sprite.cpp
@@ -1,18 +1,22 @@
-#include <SFML/Graphics/RenderWindow.hpp>
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
+
 struct game_parameters{
-  const int size_x = 800;
-  const int size_y = 800;
+  unsigned int size_x = 800;
+  unsigned int size_y = 800;
 };
 
 
 
 void display_window(){
   game_parameters obj1;
-  const int window_size = obj1.size_x;
-  sf::RenderWindow window(sf::VideoMode({800, 600}), "Sprite Game");
-};
+  sf::RenderWindow window(sf::VideoMode({obj1.size_x, obj1.size_y}), "Sprite Game");
+}
+
+
+int main(){
+  display_window();
+}
 
