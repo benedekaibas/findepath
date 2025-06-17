2af665723f4f9639afcfed582f8abeb12666f0ad
benedekaibas <kaibas.benedek02@gmail.com>
2025-06-17 21:51:30 +0200

    working function

--- games/sprite.cpp
+++ games/sprite.cpp
@@ -8,15 +8,18 @@ struct game_parameters{
   unsigned int size_y = 800;
 };
 
-
+game_parameters obj1;
 
 void display_window(){
-  game_parameters obj1;
   sf::RenderWindow window(sf::VideoMode({obj1.size_x, obj1.size_y}), "Sprite Game");
 }
 
+void display(){
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "Sprite Game");
+}
 
 int main(){
   display_window();
+  //display();
 }
 
