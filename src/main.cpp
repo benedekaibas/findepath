52aef07dd180012a1fddab152cdfd51ab60e967f
benedekaibas <kaibas.benedek02@gmail.com>
2025-08-23 13:10:48 +0200

    edit: now we receive files from folders

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <filesystem>
-
+namespace fs = std::filesystem;
 
 /*
    Finding file path with providing only the name of the given file. The code is structured in two categories: find paths in folders
-   when we know the location of the folder plus the name of the file we are looking for and find paths systemwide when we only know 
+   when we know the location of the folder plus the name of the file we are looking for and find paths systemwide when we only know
    the files name.
 */
 
@@ -12,7 +12,14 @@
 
 
 
-
+void getFileFromFolder() {
+  std::string filePath = "/home/benedek-kaibas/Documents/activity11-benedekaibas/src";
+  fs::path p(filePath);
+  
+  for(auto& f: fs::directory_iterator(p)) {
+    std::cout << f.path() << "\n";
+  }
+}
 
 
 
@@ -29,4 +36,8 @@ void setup() {
   std::string user_input;
   std::cout << "Enter the file's name you are looking for: ";
   std::cin >> user_input;
+}
+
+int main() {
+  getFileFromFolder();
 }
